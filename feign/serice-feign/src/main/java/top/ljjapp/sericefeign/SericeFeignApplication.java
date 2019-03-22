ed049878d7d3cc88a3e481672c58f5376e148cc6
LJJ <397160015@qq.com>
2019-03-22 10:09:54 +0800

    加上@EnableHystrix注解，开启熔断器

--- feign/serice-feign/src/main/java/top/ljjapp/sericefeign/SericeFeignApplication.java
+++ feign/serice-feign/src/main/java/top/ljjapp/sericefeign/SericeFeignApplication.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients//开启feign
+@EnableHystrix//开启Hysstix
 public class SericeFeignApplication {
 
     public static void main(String[] args) {
